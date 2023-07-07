c5bc9f77dd9a30cbb4a7b8f48edf04c2fcd96a11
GDIV <gdavisiv@gmail.com>
2023-07-06 21:04:24 -0400

    Edit: Added Left/Right Z-Depth

--- js/app.js
+++ js/app.js
@@ -11,7 +11,11 @@ window.addEventListener("mousemove", (e) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
 
-        let zValue = e.clientX - getComputedStyle(el).left;
+        let isInLeft = 
+            parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+        let zValue = 
+            (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+        
 
         el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
     });
