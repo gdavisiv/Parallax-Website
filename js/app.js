e90650b7b4444dca638b695b0ef85827c97546ae
GDIV <gdavisiv@gmail.com>
2023-07-06 21:11:25 -0400

    Edit: Added Z-Data to Imgs and Calculation for speedz in app.js

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ window.addEventListener("mousemove", (e) => {
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
+        let speedz = el.dataset.speedz;
 
         let isInLeft = 
             parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
@@ -17,7 +18,7 @@ window.addEventListener("mousemove", (e) => {
             (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
         
 
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue * speedz}px)`;
     });
     console.log(xValue, yValue);
 });
