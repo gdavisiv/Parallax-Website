716f0b4869f90be94cd8fa404b06c1b7bc90b8a4
GDIV <gdavisiv@gmail.com>
2023-07-07 22:54:58 -0400

    Edit: Adding Rotation

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xValue = 0, 
     yValue = 0;
 
+let rotateDegree = 0;
+
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
