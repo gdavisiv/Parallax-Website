e4c4c0d62b81964f0a8469f6fa161919a5c66c12
GDIV <gdavisiv@gmail.com>
2023-07-07 22:57:03 -0400

    Edit: Added rotate calc set (-20 to 20)

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
