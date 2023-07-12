eac31607de7e0ca37358bdebf3f76ebee3e27b4a
GDIV <gdavisiv@gmail.com>
2023-07-12 12:55:37 -0400

    Edit: Allows Pre-Loading of Page

--- js/app.js
+++ js/app.js
@@ -5,12 +5,7 @@ let xValue = 0,
 
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-    xValue = e.clientX - window.innerWidth / 2;
-    yValue = e.clientY - window.innerHeight / 2;
-
-    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
-
+function update(cursorPosition) {
     parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
@@ -20,10 +15,20 @@ window.addEventListener("mousemove", (e) => {
         let isInLeft = 
             parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
         let zValue = 
-            (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+            (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
         
 
         el.style.transform = `perspective(2300px) rotateY(${rotateDegree * rotateSpeed}deg) translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))translateZ(${zValue * speedz}px)`;
     });
-    console.log(xValue, yValue);
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+    update(e.clientX);
 });
